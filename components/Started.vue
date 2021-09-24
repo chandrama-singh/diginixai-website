be63e2f3a8743e111ed2c1198df3dd153fe5f071
Durga Prasad Mishra <dpmishraosizone@gmail.com>
2021-09-24 12:59:20 +0530

    Page edited, Component Create

---
+++ components/Started.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="section-title mt-4">
+      <h2>Lets Get Started your project</h2>
+      <p>
+        We deliver a functional professional company website within the
+        stipulated time and explain the entire process of monitoring and
+        managing it from top to bottom. Once the knowledge transfer is done,
+        you’ll be able to handle the entire content delivery process on your own
+        while you can also customize the layout whenever you want as per the
+        needs. You’ll always find us right next to you while handling any
+        technical issues, and we take it as a priority to sort it out at the
+        earliest.
+      </p>
+      <button type="button" class="btn btn-outline-info"><Nuxt-Link to="/about-us">REQUEST A QUOTE</Nuxt-Link></button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
