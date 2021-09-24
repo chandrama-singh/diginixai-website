9c24b427d9dcbb7e0f5e08443948cb1c578639fb
Abhimanyu Kushwaha <88226869+abhimanyu-kushwaha@users.noreply.github.com>
2021-09-24 14:20:15 +0530

    awards and blogs menu completed

--- pages/awards.vue
+++ pages/awards.vue
@@ -35,7 +35,7 @@
  <p style="cursor:pointer; color:darkblue;"><a>News Awards || January 4, 2021</a></p>   
 <h2>Award for Top Ranking Police & Emergency services projects in India</h2>
 <p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p>
-<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Read More</a></p>
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Countinue Reading</a></p>
 </div>
 </div>
 </div>
@@ -46,10 +46,10 @@
 </div>
 <div class="col-lg-8">
 <div class="api-info">
- <p style="cursor:pointer; color:darkblue;"><a>News Awards || January 4, 2021</a></p>   
-<h2>Award for Top Ranking Police & Emergency services projects in India</h2>
-<p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p>
-<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Read More</a></p>
+ <p style="cursor:pointer; color:darkblue;"><a>News, Awards | December 18, 2020</a></p>   
+<h2>Diginix Ai won Skoch award by government of India – highest Independent Honors Award</h2>
+<p>Received Corporate Excellence Award, 2019 at SKOCH Summit DIGINIXAI – IT Solutions came ahead...</p>
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Countinue Reading</a></p>
 </div>
 </div>
 </div>
@@ -60,10 +60,10 @@
 </div>
 <div class="col-lg-8">
 <div class="api-info">
- <p style="cursor:pointer; color:darkblue;"><a>News Awards || January 4, 2021</a></p>   
-<h2>Award for Top Ranking Police & Emergency services projects in India</h2>
-<p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p>
-<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Read More</a></p>
+ <p style="cursor:pointer; color:darkblue;"><a>Awards, News | November 30, 2020</a></p>   
+<h2>Winner of SKOCH Award and FICCI’s Homeland security Award HRMS – Diginix Ai</h2>
+<p>Diginix AI IT solutions once again outshined their counterparts and have begged one of...</p>
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Countinue Reading</a></p>
 </div>
 </div>
 </div>
@@ -74,10 +74,10 @@
 </div>
 <div class="col-lg-8">
 <div class="api-info">
- <p style="cursor:pointer; color:darkblue;"><a>News Awards || January 4, 2021</a></p>   
-<h2>Award for Top Ranking Police & Emergency services projects in India</h2>
-<p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p>
-<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Read More</a></p>
+ <p style="cursor:pointer; color:darkblue;"><a>Awards, News | December 15, 2020</a></p>   
+<h2>Diginix Ai Managed security of worlds largest fest “Kumbh Mela” with over 25 Million People</h2>
+<p>Received national award for successfully managing Kumbh 2019. ( VVIP’s and Human Resource Management,...</p>
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Countinue Reading</a></p>
 </div>
 </div>
 </div>
@@ -88,10 +88,10 @@
 </div>
 <div class="col-lg-8">
 <div class="api-info">
- <p style="cursor:pointer; color:darkblue;"><a>News Awards || January 4, 2021</a></p>   
-<h2>Award for Top Ranking Police & Emergency services projects in India</h2>
-<p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p>
-<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Read More</a></p>
+ <p style="cursor:pointer; color:darkblue;"><a>Awards, Technology | March 17, 2020</a></p>   
+<h2>FICCI Homeland Security Award for HRMS</h2>
+<p>Mr. Nishant pandey co-founder Diginix AI IT Solutions, received FICCI Homeland Security Award 2018...</p>
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Countinue Reading</a></p>
 </div>
 </div>
 </div>
@@ -102,10 +102,10 @@
 </div>
 <div class="col-lg-8">
 <div class="api-info">
- <p style="cursor:pointer; color:darkblue;"><a>News Awards || January 4, 2021</a></p>   
-<h2>Award for Top Ranking Police & Emergency services projects in India</h2>
-<p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p>
-<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Read More</a></p>
+ <p style="cursor:pointer; color:darkblue;"><a>Awards, Technology | March 17, 2020</a></p>   
+<h2>Top Ranking Police & Emergency Services Projects in India</h2>
+<p>Mr. Nishant Pandey Founder Smartetouch, Co-Founder Diginix AI IT Solutions receiving 58th SKOCH Order...</p>
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Countinue Reading</a></p>
 </div>
 </div>
 </div>
@@ -116,13 +116,32 @@
 </div>
 <div class="col-lg-8">
 <div class="api-info">
- <p style="cursor:pointer; color:darkblue;"><a>News Awards || January 4, 2021</a></p>   
-<h2>Award for Top Ranking Police & Emergency services projects in India</h2>
-<p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p>
-<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Read More</a></p>
+ <p style="cursor:pointer; color:darkblue;"><a>Awards, Technology | March 17, 2020</a></p>   
+<h2>58th SKOCH SUMMIT AWARD</h2>
+<p>Nishant Pandey, Co Founder Diginix AI IT Solution received 58th SKOCH Summit Corporate Excellence Award...</p>
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Countinue Reading</a></p>
 </div>
 </div>
 </div>
+<!----------------------row breaks----------------------->
+<div class="row">
+<div class="col-lg-4">
+<img src="~assets/images/salesforce-app.jpg" alt="Web &amp; Mobile Project" width="250" height="250">
+</div>
+<div class="col-lg-8">
+<div class="api-info">
+ <p style="cursor:pointer; color:darkblue;"><a>Application, Awards | March 16, 2020</a></p>   
+<h2>Diginix AI IT used its technology in Kumbh 2019 force deployment</h2>
+<!-- <p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p> -->
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Countinue Reading</a></p>
+</div>
+</div>
+</div>
+<section class="mt-40">
+    <Started/>
+    <Talk/>
+</section>
+
 </div>
 </section>
 
