987e43ee5cb5d482067c6b22f05aba758a8655df
Abhimanyu Kushwaha <88226869+abhimanyu-kushwaha@users.noreply.github.com>
2021-09-24 14:02:24 +0530

    awards menu complete

---
+++ pages/awards.vue
@@ -0,0 +1,141 @@
+<template>
+<div data-aos-easing="ease" data-aos-duration="1200" data-aos-delay="0">
+
+<HeaderMenu/>
+<section class="emizentech-banner banner-style-two">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-md-7">
+<div class="banner-info">
+<h1>Welcome to <span>Emizentech!</span></h1>
+<p>We are a thriving squad of nerd minds, think tankers, pro coders, business geeks, and awesomely creative beings.</p>
+<a href="enqiry.html" class="emizentech-btn"><span class="sprites-image"></span> Talk to Consultant</a>
+</div>
+</div>
+<div class="col-md-5">
+<div class="banner-image-wrap"><img src="~assets/images//about-bg.png" alt="About"></div>
+</div>
+</div>
+</div>
+</section>
+
+  <section class="emizentech-api api-spacer-two">
+<div class="container">
+<div class="section-title">
+<h2><span>Awards</span></h2>
+<!-- <p>We are competent to render robust app solutions that efficiently and specifically locate the business challenges.</p> -->
+</div>
+<!----------------------row breaks----------------------->
+<div class="row">
+<div class="col-lg-4">
+<img src="~assets/images/salesforce-app.jpg" alt="Web &amp; Mobile Project" width="250" height="250">
+</div>
+<div class="col-lg-8">
+<div class="api-info">
+ <p style="cursor:pointer; color:darkblue;"><a>News Awards || January 4, 2021</a></p>   
+<h2>Award for Top Ranking Police & Emergency services projects in India</h2>
+<p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p>
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Read More</a></p>
+</div>
+</div>
+</div>
+<!----------------------row breaks----------------------->
+<div class="row">
+<div class="col-lg-4">
+<img src="~assets/images/salesforce-app.jpg" alt="Web &amp; Mobile Project" width="250" height="250">
+</div>
+<div class="col-lg-8">
+<div class="api-info">
+ <p style="cursor:pointer; color:darkblue;"><a>News Awards || January 4, 2021</a></p>   
+<h2>Award for Top Ranking Police & Emergency services projects in India</h2>
+<p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p>
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Read More</a></p>
+</div>
+</div>
+</div>
+<!----------------------row breaks----------------------->
+<div class="row">
+<div class="col-lg-4">
+<img src="~assets/images/salesforce-app.jpg" alt="Web &amp; Mobile Project" width="250" height="250">
+</div>
+<div class="col-lg-8">
+<div class="api-info">
+ <p style="cursor:pointer; color:darkblue;"><a>News Awards || January 4, 2021</a></p>   
+<h2>Award for Top Ranking Police & Emergency services projects in India</h2>
+<p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p>
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Read More</a></p>
+</div>
+</div>
+</div>
+<!----------------------row breaks----------------------->
+<div class="row">
+<div class="col-lg-4">
+<img src="~assets/images/salesforce-app.jpg" alt="Web &amp; Mobile Project" width="250" height="250">
+</div>
+<div class="col-lg-8">
+<div class="api-info">
+ <p style="cursor:pointer; color:darkblue;"><a>News Awards || January 4, 2021</a></p>   
+<h2>Award for Top Ranking Police & Emergency services projects in India</h2>
+<p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p>
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Read More</a></p>
+</div>
+</div>
+</div>
+<!----------------------row breaks----------------------->
+<div class="row">
+<div class="col-lg-4">
+<img src="~assets/images/salesforce-app.jpg" alt="Web &amp; Mobile Project" width="250" height="250">
+</div>
+<div class="col-lg-8">
+<div class="api-info">
+ <p style="cursor:pointer; color:darkblue;"><a>News Awards || January 4, 2021</a></p>   
+<h2>Award for Top Ranking Police & Emergency services projects in India</h2>
+<p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p>
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Read More</a></p>
+</div>
+</div>
+</div>
+<!----------------------row breaks----------------------->
+<div class="row">
+<div class="col-lg-4">
+<img src="~assets/images/salesforce-app.jpg" alt="Web &amp; Mobile Project" width="250" height="250">
+</div>
+<div class="col-lg-8">
+<div class="api-info">
+ <p style="cursor:pointer; color:darkblue;"><a>News Awards || January 4, 2021</a></p>   
+<h2>Award for Top Ranking Police & Emergency services projects in India</h2>
+<p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p>
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Read More</a></p>
+</div>
+</div>
+</div>
+<!----------------------row breaks----------------------->
+<div class="row">
+<div class="col-lg-4">
+<img src="~assets/images/salesforce-app.jpg" alt="Web &amp; Mobile Project" width="250" height="250">
+</div>
+<div class="col-lg-8">
+<div class="api-info">
+ <p style="cursor:pointer; color:darkblue;"><a>News Awards || January 4, 2021</a></p>   
+<h2>Award for Top Ranking Police & Emergency services projects in India</h2>
+<p>The state of Uttar Pradesh, the largest state in terms of population with roughly...</p>
+<p><a href="#" class="emizentech-btn emizentech-small-btn text-white text-decoration-none">Read More</a></p>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<FooterMenu/>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
