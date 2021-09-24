ec85114136f8ef26187755b2d45c02c04e72896f
Abhimanyu Kushwaha <88226869+abhimanyu-kushwaha@users.noreply.github.com>
2021-09-24 11:09:42 +0530

    .

--- pages/uowd.vue
+++ pages/uowd.vue
@@ -23,7 +23,7 @@
 <div class="container">
 <div class="section-title">
 <h2><span>UOWD </span>DISCOUNTS</h2>
-<p>Let us tell you the story of our formation</p>
+<!-- <p>Let us tell you the story of our formation</p> -->
 </div>
 <div class="small-title">
 <p>Get any type of it product developed at lowest possible cost. We will support you with consulting and development to bring your idea to life.</p>
@@ -150,7 +150,7 @@
 </div>
 </section>
 
-<section class="emizentech-blog">
+<!-- <section class="emizentech-blog">
 <div class="container">
 <div class="row">
 <div class="col-md-6">
@@ -196,56 +196,10 @@
 </div>
 </div>
 </div>
-</section>
-
-<section class="emizentech-clients">
-<div class="container">
-<div class="owl-carousel owl-loaded owl-drag" data-items="7" data-items-tablet="5" data-items-mobile-landscape="4" data-items-mobile-portrait="3" data-margin="15" data-loop="true" data-nav="false" data-dots="false" data-autoplay="true">
+</section> -->
 
 
-
-
-
-
-
-
-<div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(-931px, 0px, 0px); transition: all 0.6s ease 0s; width: 2980px;"><div class="owl-item cloned" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client5.png" alt="Client" width="65" height="65"></div>
-</div></div><div class="owl-item cloned" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client6.png" alt="Client" width="64" height="65"></div>
-</div></div><div class="owl-item cloned" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client7.png" alt="Client" width="116" height="65"></div>
-</div></div><div class="owl-item cloned" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client4.png" alt="Client" width="120" height="65"></div>
-</div></div><div class="owl-item" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client1.png" alt="Client" width="117" height="65"></div>
-</div></div><div class="owl-item active" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client2.png" alt="Client" width="111" height="65"></div>
-</div></div><div class="owl-item active" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client3.png" alt="Client" width="108" height="65"></div>
-</div></div><div class="owl-item active" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client4.png" alt="Client" width="120" height="65"></div>
-</div></div><div class="owl-item active" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client5.png" alt="Client" width="65" height="65"></div>
-</div></div><div class="owl-item" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client6.png" alt="Client" width="64" height="65"></div>
-</div></div><div class="owl-item" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client7.png" alt="Client" width="116" height="65"></div>
-</div></div><div class="owl-item" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client4.png" alt="Client" width="120" height="65"></div>
-</div></div><div class="owl-item cloned" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client1.png" alt="Client" width="117" height="65"></div>
-</div></div><div class="owl-item cloned" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client2.png" alt="Client" width="111" height="65"></div>
-</div></div><div class="owl-item cloned" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client3.png" alt="Client" width="108" height="65"></div>
-</div></div><div class="owl-item cloned" style="width: 171.25px; margin-right: 15px;"><div class="item">
-<div class="emizentech-image"><img src="~/assets/images//client4.png" alt="Client" width="120" height="65"></div>
-</div></div></div></div><div class="owl-nav disabled"><div class="owl-prev"><div class="slider-no-current"><span class="current-no">0</span><span class="total-no">0</span></div><span class="current-monials"></span></div><div class="owl-next"><div class="slider-no-next"></div><span class="next-monials"></span></div></div><div class="owl-dots disabled"></div></div>
-</div>
-</section>
-
-<section class="emizentech-shy aos-init aos-animate" data-aos="fade-up">
+<!-- <section class="emizentech-shy aos-init aos-animate" data-aos="fade-up">
 <div class="container">
 <h3>Don’t be Shy</h3>
 <h2>Say Hello!</h2>
@@ -253,196 +207,23 @@
 <div class="text-right"><a href="enqiry.html" class="emizentech-btn emizentech-medium-btn">Get in Touch</a></div>
 </div>
 </div>
+</section> -->
+
+<section class="emizentech-about about-style-five" id="first-section">
+<div class="container">
+<div class="section-title">
+<h2>Lets Get Started
+your project</h2>
+<!-- <p>Let us tell you the story of our formation</p> -->
+</div>
+<div class="small-title">
+<p>We focus strongly on function and usability and train you to manage your website as a business asset. We go to great lengths to assure your content is delivered in such a way that visitors can easily and intuitively find the information they seek, resulting in a pleasant and productive user experience.</p>
+<div class="text-center">
+<div class="text-center"><a href="#" class="emizentech-btn emizentech-medium-btn">Request A Quote</a></div>
+</div>
+</div>
+</div>
 </section>
-
-<!-- <section class="home-contactus">
-              <div class="container">
-                <div class="row">
-                  <div class="col-sm-6">
-                    <div
-                      class="company-info wow fadeInDown"
-                      data-wow-delay="0.1s"
-                    >
-                      <h3 class="getintouch-heading">Let’s Talk Business</h3>
-                      <p>
-                        Reach out to us by below-given details, or drop us a
-                        text to start a conversation, we are here to provide
-                        best in class business solutions.
-                      </p>
-                      <ul class="banner_certi">
-                        <li>
-                          <img
-                            src="images/megento_2trained.png"
-                            alt="Magento 2 Partner"
-                            width="67"
-                            height="131"
-                          />
-                        </li>
-                        <li>
-                          <img
-                            src="images/magento2-associate-develope-1.png"
-                            alt="Magento 2 Associate Developer"
-                            width="67"
-                            height="131"
-                          />
-                        </li>
-                        <li>
-                          <img
-                            src="images/magento-2-solutions-specialist-1.png"
-                            alt="Magento 2 Solutions Specialist"
-                            width="67"
-                            height="131"
-                          />
-                        </li>
-                        <li>
-                          <img
-                            src="images/magento2-ecomerce-1.png"
-                            alt="Magento 2 Ecomerce"
-                            width="67"
-                            height="131"
-                          />
-                        </li>
-                        <li>
-                          <img
-                            src="images/adobe-solution-footer-1.png"
-                            alt="Adobe Solution Partner"
-                            width="67"
-                            height="131"
-                          />
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div class="col-sm-6">
-                    <div
-                      class="contact-form wow fadeInUp"
-                      data-wow-delay="0.2s"
-                    >
-                      <h3>SAY SOMETHING</h3>
-                      <div
-                        role="form"
-                        class="wpcf7"
-                        id="wpcf7-f884-p55-o2"
-                        lang="en-US"
-                        dir="ltr"
-                      >
-                        <div
-                          class="screen-reader-response"
-                          role="alert"
-                          aria-live="polite"
-                        ></div>
-                        <form
-                          action="/blog/#wpcf7-f884-p55-o2"
-                          method="post"
-                          class="wpcf7-form init"
-                          novalidate="novalidate"
-                        >
-                          <div style="display: none">
-                            <input type="hidden" name="_wpcf7" value="884" />
-                            <input
-                              type="hidden"
-                              name="_wpcf7_version"
-                              value="5.2"
-                            />
-                            <input
-                              type="hidden"
-                              name="_wpcf7_locale"
-                              value="en_US"
-                            />
-                            <input
-                              type="hidden"
-                              name="_wpcf7_unit_tag"
-                              value="wpcf7-f884-p55-o2"
-                            />
-                            <input
-                              type="hidden"
-                              name="_wpcf7_container_post"
-                              value="55"
-                            />
-                            <input
-                              type="hidden"
-                              name="_wpcf7_posted_data_hash"
-                              value
-                            />
-                            <input
-                              type="hidden"
-                              name="_wpcf7_recaptcha_response"
-                              value="03AGdBq26LRDgWQkdMtx1e3-Q_i6hmnrXNnEiMMA40-3QazhC0jx6EYpBId0eVP6t1RwTBH45PFmPWx1nSvFhrSUV-sHkSwq4oPhp6M7lJDxxYSxXX8RYMk25fLgdNz6W_SM5Np-fgOpXXqMj3tbUnWodFxhF5No4nKgy5xDZ50Yemy-lTrEgf6URP93qYyH1xwZno1psNPKq3kUR4EiZ6lopmgbN-w4dYyHEa3HhEerWWUiMgshPEzMRfl9dLDS6jyPDHiiBE5gONjUTbkAxwK_z-vfYpFfibZCChGLexLdotyKsVi_2Ea-1gzFXUnf2KNj3dG4eL5JDGImmt7HN3mKuRPKXggv9t0tevwD2AGLNmZSleFvsJJWWzEMs0-ZkPWzg8HFBy-qOJ9RWrHsQCF7p_GPZh591bornukenghgTpp1q2KNbdkFwN0Lwsxm6vkttoGVuJqMmB"
-                            />
-                          </div>
-                          <p>
-                            <label
-                              ><br />
-                              <span class="wpcf7-form-control-wrap your-name"
-                                ><input
-                                  type="text"
-                                  name="your-name"
-                                  value
-                                  size="40"
-                                  class="
-                                    wpcf7-form-control
-                                    wpcf7-text
-                                    wpcf7-validates-as-required
-                                  "
-                                  aria-required="true"
-                                  aria-invalid="false"
-                                  placeholder="Enter name"
-                              /></span> </label
-                            ><br />
-                            <label
-                              ><br />
-                              <span class="wpcf7-form-control-wrap your-email"
-                                ><input
-                                  type="email"
-                                  name="your-email"
-                                  value
-                                  size="40"
-                                  class="
-                                    wpcf7-form-control
-                                    wpcf7-text
-                                    wpcf7-email
-                                    wpcf7-validates-as-required
-                                    wpcf7-validates-as-email
-                                  "
-                                  aria-required="true"
-                                  aria-invalid="false"
-                                  placeholder="Enter email"
-                              /></span> </label
-                            ><br />
-                            <label
-                              ><br />
-                              <span
-                                class="wpcf7-form-control-wrap your-message"
-                              >
-                                <textarea
-                                  name="your-message"
-                                  cols="40"
-                                  rows="10"
-                                  class="wpcf7-form-control wpcf7-textarea"
-                                  aria-invalid="false"
-                                  placeholder="Enter your message"
-                                ></textarea>
-                              </span> </label
-                            ><br />
-                            <input
-                              type="submit"
-                              value="Submit"
-                              class="wpcf7-form-control wpcf7-submit btnstyle2"
-                            /><span class="ajax-loader"></span>
-                          </p>
-                          <div
-                            class="wpcf7-response-output"
-                            role="alert"
-                            aria-hidden="true"
-                          ></div>
-                        </form>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section> -->
-
 <FooterMenu/>
 </div>
 </template>
