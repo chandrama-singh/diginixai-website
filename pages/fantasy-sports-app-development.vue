577a6eb5154b91062d66c1fb2dec0cd427b77724
Abhimanyu Kushwaha <88226869+abhimanyu-kushwaha@users.noreply.github.com>
2021-09-24 17:31:45 +0530

    services menu completed

--- pages/fantasy-sports-app-development.vue
+++ pages/fantasy-sports-app-development.vue
@@ -1,5 +1,381 @@
 <template>
-  
+  <div data-aos-easing="ease" data-aos-duration="1200" data-aos-delay="0">
+
+<HeaderMenu/>
+
+<section class="emizentech-banner banner-style-two">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-md-7">
+<div class="banner-info">
+<h1>Fantasy Sports App <br> <span>Development</span></h1>
+<p>Provide your users with a highly-engulfing, potent, and thrilling fantasy sports league playing experience with Diginixai reliable, scalable, and cost-effective fantasy sports app development services.</p>
+<!-- <a href="enqiry.html" class="emizentech-btn"><span class="sprites-image"></span> Talk to Consultant</a> -->
+</div>
+</div>
+<div class="col-md-5">
+<div class="banner-image-wrap"><img src="~/assets/images//react-native3.jpg" alt="React"></div>
+</div>
+</div>
+</div>
+</section>
+
+<section class="emizentech-about about-style-five" id="first-section">
+<div class="container">
+<div class="section-title">
+<h2><span>THE RAPID GROWTH OF </span>FANTASY SPORTS APP MARKET</h2>
+<!-- <p>Develop future-proof mobile application solutions by hiring full-stack React Native developers from Emizentech.</p> -->
+</div>
+<div class="small-title">
+<p>With the advancement in digital technology and the emergence of various T20 cricket leagues, we all have witnessed a massive upsurge in the demand of fantasy cricket apps where users can get a rich experience and also earn some reward while playing the game.</p>
+<p>Undeniably, the fantasy sports app domain has emerged as one of the biggest ventures and it is continually growing by leaps and bounds. Moreover, the level of exuberance, fun, anticipation, and rewards make the fantasy sports app domain more enthralling and attention-grabbing for users.</p>
+<p>The concept of fantasy sports is what makes it stand apart from the rest. Dream 11 is one fine example of that. It is a game in which users have to form their own imaginary team by selecting players as per the rules and play either a single match or the entire league as per their preference.</p>
+</div>
+</div>
+</section>
+
+<section class="emizentech-about about-style-five" id="first-section">
+<div class="container">
+<div class="section-title">
+<h2><span>FANTASY APP SPORTS </span>SOFTWARE DEVELOPMENT</h2>
+<!-- <p>Develop future-proof mobile application solutions by hiring full-stack React Native developers from Emizentech.</p> -->
+</div>
+<div class="small-title">
+<p>At Diginixai, we hold expertise in carving the most robust, state-of-the-art, and utterly engaging fantasy sports app solutions that help to build brand awareness, satiate users’ entertainment needs, easily scalable whenever you want. When it comes to offering captivating fantasy sports app development services, we are ahead of competitors by leaps and bounds.</p>
+<p>Over the years, Diginixai team of application developers has created a commendable range of custom fantasy app solutions that are packed with the most intriguing and highly advanced features. We have successfully served a large clientele base across the world and continuing to do so. If you have any type of dream plan to build your own fantasy sports app or fantasy sports software solutions, rope in Emizentech fantasy sports app development services to get a feature-rich fantasy sports app with enchanting user-interface and immersive user-experience.</p>
+</div>
+</div>
+</section>
+<!-- <section class="emizentech-api api-style-two api-spacer-two">
+<div class="container">
+<div class="api-info">
+<div class="row">
+<div class="col-md-4 col-6">
+<div class="api-item">
+<div class="emizentech-icon"><img src="~/assets/images//developers.png" alt="Developers" width="34" height="30"></div>
+<h4>Certified Developers</h4>
+</div>
+</div>
+<div class="col-md-4 col-6">
+<div class="api-item">
+<div class="emizentech-icon"><img src="~/assets/images//secure.png" alt="Secure" width="24" height="30"></div>
+<h4>Secure with NDA</h4>
+</div>
+</div>
+<div class="col-md-4 col-6">
+<div class="api-item">
+<div class="emizentech-icon"><img src="~/assets/images//buy.png" alt="Buy"></div>
+<h4>Try &amp; Buy Model</h4>
+</div>
+</div>
+<div class="col-md-4 col-6">
+<div class="api-item">
+<div class="emizentech-icon"><img src="~/assets/images//refund.png" alt="Refund"></div>
+<h4>7 Day Refund Policy</h4>
+</div>
+</div>
+<div class="col-md-4 col-6">
+<div class="api-item">
+<div class="emizentech-icon"><img src="~/assets/images//flexible.png" alt="Flexible"></div>
+<h4>Flexible Working Hours</h4>
+</div>
+</div>
+<div class="col-md-4 col-6">
+<div class="api-item">
+<div class="emizentech-icon"><img src="~/assets/images//support.png" alt="Support"></div>
+<h4>24 X 7 Support</h4>
+</div>
+</div>
+<div class="col-md-4 col-6">
+<div class="api-item">
+<div class="emizentech-icon"><img src="~/assets/images//security.png" alt="Security"></div>
+<h4>Security &amp; IP Protection</h4>
+</div>
+</div>
+<div class="col-md-4 col-6">
+<div class="api-item">
+<div class="emizentech-icon"><img src="~/assets/images//source.png" alt="Source"></div>
+<h4>Source Code Authorization</h4>
+</div>
+</div>
+<div class="col-md-4 col-6">
+<div class="api-item">
+<div class="emizentech-icon"><img src="~/assets/images//precise.png" alt="Precise"></div>
+<h4>Precise Reporting</h4>
+</div>
+</div>
+<div class="col-md-4 col-6">
+ <div class="api-item">
+<div class="emizentech-icon"><img src="~/assets/images//beacon.png" alt="Beacon"></div>
+<h4>Quick Team Scaling</h4>
+</div>
+</div>
+<div class="col-md-4 col-6">
+<div class="api-item">
+<div class="emizentech-icon"><img src="~/assets/images//interviews.png" alt="Interviews"></div>
+<h4>Conduct Interviews</h4>
+</div>
+</div>
+<div class="col-md-4 col-6">
+<div class="api-item">
+<div class="emizentech-icon"><img src="~/assets/images//engagement.png" alt="Engagement"></div>
+<h4>Multiple Engagement Models</h4>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section> -->
+
+<section class="emizentech-development">
+<div class="container">
+<div class="section-title">
+<h2><span>FOR </span>USER</h2>
+<!-- <p>There is a myriad of reasons to rope in our online grocery app development services to get your best online grocery shopping app. Some of the key reasons are –</p> -->
+</div>
+<div class="row">
+<div class="col-lg-6">
+<div class="development-item development-spacer">
+<!-- <div class="emizentech-image"><img src="~/assets/images//engagement_1.png" alt="Engagement"></div> -->
+<!-- <h3>Engagement<span> Model</span></h3> -->
+<!-- <p>Our React Native engagement models rely on a particularity mentioning - the deliverables, budget, and timeline to assure that all the requirements are attained within a predefined timeline.</p> -->
+<div class="development-inner">
+<div class="development-item-wrap">
+<div class="emizentech-icon"><img src="~/assets/images//icon43.png" alt="Onsite"></div>
+<div class="development-info">
+<h5>Easy to Install and Login</h5>
+<p>You just need to install the app to use it. There is no long and dull registration process to access the app. Install the application and the home screen will pop up.
+</p>
+</div>
+</div>
+<div class="development-item-wrap">
+<div class="emizentech-icon"><img src="~/assets/images//icon44.png" alt="Offsite"></div>
+<div class="development-info">
+<h5>Feature Rich Dashboard</h5>
+<p>On the dashboard, you can see the record of live, upcoming, and finished matches along with the contests you are a part of.</p>
+</div>
+</div>
+<div class="development-item-wrap">
+<div class="emizentech-icon"><img src="~/assets/images//icon44.png" alt="Offsite"></div>
+<div class="development-info">
+<h5>Create or Enter the Contest</h5>
+<p>Users have dual options of either creating a contest or entering into the prevailing contests by paying the requested entry fee.</p>
+</div>
+</div>
+<div class="development-item-wrap">
+<div class="emizentech-icon"><img src="~/assets/images//icon44.png" alt="Offsite"></div>
+<div class="development-info">
+<h5>Create Your Own Fanstasy Team</h5>
+<p>Once you entered into a contest, you can create your own fantasy team by selecting the coveted players and following the selection guidelines.</p>
+</div>
+</div>
+<div class="development-item-wrap">
+<div class="emizentech-icon"><img src="~/assets/images//icon44.png" alt="Offsite"></div>
+<div class="development-info">
+<h5>Team Preview</h5>
+<p>Before the contest begins, users can preview the team they have carved after entering the contest. They can also make significant changes before the contest rolls out.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-6">
+<div class="development-item">
+<!-- <div class="emizentech-image"><img src="~/assets/images//pricing.png" alt="Pricing"></div> -->
+<!-- <h3>Pricing <span>Model</span></h3> -->
+<!-- <p>At Emizentech, we have a React Native pricing model for our clients that helps us in revealing our transparent agreement to our clients.</p> -->
+<div class="development-inner">
+<div class="development-item-wrap">
+<div class="emizentech-icon"><img src="~/assets/images//icon45.png" alt="Hybrid"></div>
+<div class="development-info">
+<h5>Assign Role</h5>
+<p>In addition to team preview, you can also assign different roles to different team members keeping in mind their capabilities.</p>
+</div>
+</div>
+<div class="development-item-wrap">
+<div class="emizentech-icon"><img src="~/assets/images//dedicated.png" alt="Dedicated"></div>
+<div class="development-info">
+ <h5>Additional Attributes</h5>
+<p>In more tab, you can find an array of options. Here you will find add or withdraw cash options, refer a friend option, how to play, terms & conditions, privacy policy, FAQs, and the list goes on.</p>
+</div>
+</div>
+<div class="development-item-wrap">
+<div class="emizentech-icon"><img src="~/assets/images//time-material.png" alt="Time"></div>
+<div class="development-info">
+<h5>Verification</h5>
+<p>You can verify your mobile and email address in verification tab.</p>
+</div>
+</div>
+<div class="development-item-wrap">
+<div class="emizentech-icon"><img src="~/assets/images//time-material.png" alt="Time"></div>
+<div class="development-info">
+<h5>add Cash</h5>
+<p>To play the fantasy cricket game uninterruptedly, you have to add cash in add cash tab according to league charges. Amount can be added through different ways like debit/credit card, net banking, and UPI.</p>
+</div>
+</div>
+<div class="development-item-wrap">
+<div class="emizentech-icon"><img src="~/assets/images//time-material.png" alt="Time"></div>
+<div class="development-info">
+<h5>Refer a Friend</h5>
+<p>You can also refer friends and earn bonus by sharing invite codes with friends.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+
+<section class="emizentech-cloud cloud-spacer-two cloud-style-four">
+<div class="container">
+<div class="cloud-item">
+<div class="row align-items-center">
+<div class="col-lg-5">
+<div class="emizentech-image"><img src="~/assets/images//icon138.png" alt="3dcart Integration"></div>
+</div>
+<div class="col-lg-7">
+<div class="cloud-info">
+<h3><span>KEY ATTRIBUTES OF FANTASY </span>SPORTS MOBILE APP</h3>
+<p>We have stated above that we offer a profound selection of fantasy sports app solutions that are super potent and take the gaming experience for users a notch higher. Let’s get conversant with some of the key features of the Diginixai fantasy sports mobile app.</p>
+<div class="row">
+<div class="col-md-12">
+<ul class="bullet-list">
+<li>User-Verification</li>
+<li>Join Contest</li>
+<li>Create Contest</li>
+<li>Add & Withdrawl Cash</li>
+<li>Invite & Earn</li>
+<li>Playing History</li>
+<li>Winning History</li>
+<li>Refer Friends</li>
+<li>My Wallet</li>
+<li>Team Preview</li>
+<li>User Dashboard</li>
+</ul>
+</div>
+</div>
+<!-- <a href="enqiry.html" class="emizentech-btn"><span class="sprites-image"></span> Talk to Consultant</a> -->
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<!-- <section class="emizentech-api api-spacer-two">
+<div class="container">
+<div class="section-title">
+<h2><span>Benefits of Hiring</span> Certified React Native Developers</h2>
+<p>Hire our expert front-end React Native experts to get the assurance that your app is in the experienced and skilled hands.</p>
+</div>
+<div class="row">
+<div class="col-lg-4">
+<h3><span>W</span>e ensure excellence in our application development with accuracy, speed, and customer satisfaction.</h3>
+</div>
+<div class="col-lg-8">
+<div class="api-info">
+<p>The offshore React Native developers offer a comprehensive package of the development management services. When the services are carried by the remote employees, the brand is allowed to add more effort and target more on their best working elements for growth enhancement. The dedicated React Native developers render the projects on-time with no flaws or errors. The clients are always free to pick the developer as per their project needs when they move ahead to hire dedicated developers.</p>
+<p>Hiring React Native developers save a lot of money as the engagement models the company offers permit to choose the resources according to the needs and budget. It proves to be cost-effective as no investment is needed for a physical office. Also, the development cost is attained at comparatively affordable prices than native development prices. As the client gets connected with an offshore development company, they are assigned a team of project managers, developers, designers, and architects. This takes the focus of the client on his business process by putting the entire responsibility on the shoulders of the dedicated developers to render quality software. The client is offered a chance to pick the most passionate and best candidate from a pool of skilled dedicated developers, and they attain a more productive result comparatively.</p>
+</div>
+</div>
+</div>
+</div>
+</section> -->
+
+<section class="emizentech-services services-style-two" id="skip-section">
+<div class="container">
+<div class="section-title">
+<h2><span>Services</span></h2>
+<!-- <p>Hire React Native developers from Emizentech and develop a robust and reliable application for your business to meet the desired outcomes.</p> -->
+</div>
+<div class="row justify-content-center">
+<div class="service-item">
+<img src="~/assets/images//icon87.png" alt="Magento">
+<h4>Rapid Delivery</h4>
+</div>
+<div class="service-item">
+<img src="~/assets/images//icon88.png" alt="3dCart">
+<h4>Proficient and <br> Skilled Developers</h4>
+</div>
+<div class="service-item">
+<img src="~/assets/images//icon89.png" alt="Magento">
+<h4>Agile Methodologies</h4>
+</div>
+<div class="service-item">
+<img src="~/assets/images//icon90.png" alt="Magento">
+<h4>Extensively Customer Vouched Reputation</h4>
+</div>
+<div class="service-item">
+<img src="~/assets/images//icon91.png" alt="Magento">
+<h4>100% Customer Satisfaction</h4>
+</div>
+<div class="service-item">
+<img src="~/assets/images//icon92.png" alt="Magento">
+<h4>Competitive Pricing</h4>
+</div>
+<div class="service-item">
+<img src="~/assets/images//icon93.png" alt="Magento">
+<h4>Nasscom Certified Company</h4>
+</div>
+<div class="service-item">
+<img src="~/assets/images//icon94.png" alt="Magento">
+<h4>Active Support And maintenance</h4>
+</div>
+<div class="service-item">
+<img src="~/assets/images//icon95.png" alt="Magento">
+<h4>The Best <br> React Native Developer</h4>
+</div>
+<div class="service-item">
+<img src="~/assets/images//icon96.png" alt="Magento">
+<h4>Service Global Clients</h4>
+</div>
+<div class="service-item">
+<img src="~/assets/images//icon97.png" alt="Magento">
+<h4>ISO 27001:2013 Certified Company</h4>
+</div>
+</div>
+</div>
+</section>
+<section class="emizentech-cloud cloud-spacer-two cloud-style-four">
+<div class="container">
+<div class="cloud-item">
+<div class="row align-items-center">
+<div class="col-lg-5">
+<div class="emizentech-image"><img src="~/assets/images//icon138.png" alt="3dcart Integration"></div>
+</div>
+<div class="col-lg-7">
+<div class="cloud-info">
+<h3><span>MAJOR BENEFITS OF FANTASY </span>SPORTS APP</h3>
+<p>It would be quite daunting for us to describe all the benefits of using a fantasy sports app here. However, we will state the few major ones to make you conversant with the untold benefits of the fantasy sports app.</p>
+<div class="row">
+<div class="col-md-12">
+<ul class="bullet-list">
+<li>Amplify product, services, and brand awareness to a great extent.</li>
+<li>Increase sales and conversions by leaps and bounds.</li>
+<li>You can build better and long-lasting relationship with customers.</li>
+<li>Bring a profound amount of traffic to the website.</li>
+<li>Helps in garnering database of new customers.</li>
+<li>Help you build a loyal online community.</li>
+<li>Dwindle the dependency on third party for playing.</li>
+<li>Provides a stunning opportunity to earn huge income via AdSense.</li>
+</ul>
+</div>
+</div>
+<!-- <a href="enqiry.html" class="emizentech-btn"><span class="sprites-image"></span> Talk to Consultant</a> -->
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<Started/>
+<Stats/>
+<Footer/>
+  </div>
 </template>
 
 <script>
